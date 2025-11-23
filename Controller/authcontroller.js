ea07e1bb6bf7df694e4ad237244d396e7f4bace4
Abhay Kadu <abhay@triplewsols.com>
2025-11-24 01:25:12 +0530

    mergeupdate

--- Controller/authcontroller.js
+++ Controller/authcontroller.js
@@ -16,10 +16,10 @@ const Login=async (req, res) => {
     if (!validPassword) return res.status(400).json({ message: "Invalid email or password" });
 
     const token = jwt.sign({ id: user._id },'TOKEN', { expiresIn: "10h" });
-    setTimeout(()=>{
+    
     res.status(200).json({ message: "Login successful", token,username:user?.username,id:user?.id });
 
-    },2000)
+    
 
   } catch (error) {
     res.status(500).json({ error: error.message });
